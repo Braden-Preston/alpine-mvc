75ec5157e040eb1af891b647b71726192ccdfc7e
Braden Preston <bpfilmsinc@gmail.com>
2021-10-16 04:01:41 -0500

    add esbuild to bundle source

--- app/index.js
+++ app/index.js
@@ -1,7 +1,6 @@
-'use strict'
-
-module.exports = async function (fastify, opts) {
-  fastify.get('/', async function (request, reply) {
-    return 'this is an example app'
-  })
-}
+module.exports = async function (app, _opts) {
+  app.get("/", async function (req, res) {
+    console.log(Object.keys(req.id))
+    return "this is an example apps";
+  });
+};
