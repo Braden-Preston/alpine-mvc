3524c254cfc41a1c0f64770cf85108a49c558145
Braden Preston <bpfilmsinc@gmail.com>
2021-10-16 05:10:41 -0500

    statically host public directory

--- app/index.js
+++ app/index.js
@@ -1,6 +1,9 @@
-module.exports = async function (app, _opts) {
-  app.get("/", async function (req, res) {
-    console.log(Object.keys(req.id))
-    return "this is an example apps";
+let path = require("path");
+
+module.exports = async function (app, opts) {
+  app.register(require("fastify-static"), {
+    root: path.join(__dirname, "..", "public")
   });
+
+  app.register(require("./router"));
 };
